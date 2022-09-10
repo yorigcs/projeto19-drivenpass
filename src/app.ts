6632d57fd8dcce67b37a3178d019379a5270fbe1
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-10 12:18:42 -0300

    feat: implements express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
-const app = (): void => {
-  console.log('init project')
-}
-app()
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use([cors(), express.json()])
+app.get('/', (req, res) => res.send('ok'))
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
