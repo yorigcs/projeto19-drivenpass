e74e1408a220e1a5c4f9e8081a394be09a2aaf37
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 15:52:37 -0300

    feat: implements signUpService in SignUpController

--- src/controllers/signUpController.ts
+++ src/controllers/signUpController.ts
@@ -1,14 +1,25 @@
 import { Response, Request } from 'express'
 import { Controller } from '../protocols'
 import { signUpSchema } from '../schemas'
+import { signUpService } from '../services'
 import { ValidatorAdapter } from '../utils/validatorAdapter'
+
+interface signUpRequest extends Request {
+  body: {
+    email: string
+    password: string
+    confirmPassword: string
+  }
+}
+
 export class SignUpController implements Controller {
-  async handle (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+  async handle (req: signUpRequest, res: Response): Promise<Response<any, Record<string, any>>> {
     const validatorAdapter = new ValidatorAdapter(signUpSchema, req.body)
     const validResp = validatorAdapter.validate()
     if (validResp) {
       return res.status(validResp.statusCode).json(validResp.body)
     }
-    return res.send('okay')
+    const serviceResponse = await signUpService(req.body)
+    return res.status(serviceResponse.statusCode).json(serviceResponse.body)
   }
 }
