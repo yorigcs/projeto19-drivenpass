a272b1cd2424a69e39352e92cebb96a64cad38ea
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 18:29:12 -0300

    feat: create signInSchema

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -5,3 +5,8 @@ export const signUpSchema = Joi.object({
   password: Joi.string().required(),
   confirmPassword: Joi.string().required()
 })
+
+export const signInSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+})
