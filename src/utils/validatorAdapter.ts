af9ce9df581714f73bca3ecbe6c94dbf6564b0df
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 02:46:10 -0300

    feat: create validatorAdapter to handle Requests

---
+++ src/utils/validatorAdapter.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi'
+import { InvalidParamError } from '../errors'
+import { unprocessableEntity } from '../helpers'
+import { HttpResponse } from '../protocols'
+
+interface Data {
+  [key: string]: number | string
+}
+export class ValidatorAdapter {
+  private readonly schema: Joi.ObjectSchema
+  private readonly data: Data
+
+  constructor (schema: Joi.ObjectSchema, data: Data) {
+    this.schema = schema
+    this.data = data
+  }
+
+  validate (): HttpResponse {
+    const { error } = this.schema.validate(this.data, { abortEarly: false })
+    if (error) {
+      return unprocessableEntity(new InvalidParamError(error.message))
+    }
+  }
+}
