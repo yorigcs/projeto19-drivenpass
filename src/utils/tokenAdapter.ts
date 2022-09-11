da5fe881d22c1cbee6379a338ddf72d10a95633f
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 20:39:57 -0300

    feat: create verifyToken method

--- src/utils/tokenAdapter.ts
+++ src/utils/tokenAdapter.ts
@@ -3,15 +3,27 @@ import { User } from '@prisma/client'
 import dotenv from 'dotenv'
 dotenv.config()
 
+interface Payload {
+  id: string
+  email: string
+  iat: number
+  exp: number
+}
+
 export class TokenAdapter {
-  private readonly user: User
   private readonly SECRET = process.env.SECRET_JWT || 'secret@secret'
   private readonly EXP_TIME = process.env.EXP_JWT || '1d'
-  constructor (user: User) {
-    this.user = user
+
+  generateToken (user: User): string {
+    return jwt.sign(user, this.SECRET, { expiresIn: this.EXP_TIME })
   }
 
-  generateToken (): string {
-    return jwt.sign(this.user, this.SECRET, { expiresIn: this.EXP_TIME })
+  verifyToken (token: string): Payload | null {
+    try {
+      const decoded = jwt.verify(token, this.SECRET) as Payload
+      return decoded
+    } catch (error) {
+      return null
+    }
   }
 }
