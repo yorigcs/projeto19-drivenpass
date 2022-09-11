d78bc7f271a3852b78327db447455b74dcc258ee
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 18:30:55 -0300

    feat: create unauthorized, conflit and serverError helpers

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -1,16 +1,5 @@
+import { ServerError } from '../errors/serverError'
 import { HttpResponse } from '../protocols'
-
-export const badRequest = (data: any): HttpResponse => ({
-  statusCode: 400,
-  body: data
-
-})
-
-export const unprocessableEntity = (data: any): HttpResponse => ({
-  statusCode: 422,
-  body: data
-
-})
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
@@ -21,5 +10,35 @@ export const ok = (data: any): HttpResponse => ({
 export const created = (data: any): HttpResponse => ({
   statusCode: 201,
   body: data
+
+})
+
+export const badRequest = (data: any): HttpResponse => ({
+  statusCode: 400,
+  body: data
+
+})
+
+export const unauthorized = (data: any): HttpResponse => ({
+  statusCode: 401,
+  body: data
+
+})
+
+export const conflict = (data: any): HttpResponse => ({
+  statusCode: 409,
+  body: data
+
+})
+
+export const unprocessableEntity = (data: any): HttpResponse => ({
+  statusCode: 422,
+  body: data
+
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 
 })
