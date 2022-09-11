0cf80861a06ba423a04719abae9f23f0f0686082
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 02:48:25 -0300

    feat: implements validatorAdapter to ensure schema params is provided

--- src/controllers/signUpController.ts
+++ src/controllers/signUpController.ts
@@ -1,8 +1,14 @@
 import { Response, Request } from 'express'
 import { Controller } from '../protocols'
-
+import { signUpSchema } from '../schemas'
+import { ValidatorAdapter } from '../utils/validatorAdapter'
 export class SignUpController implements Controller {
   async handle (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const validatorAdapter = new ValidatorAdapter(signUpSchema, req.body)
+    const validResp = validatorAdapter.validate()
+    if (validResp) {
+      return res.status(validResp.statusCode).json(validResp.body)
+    }
     return res.send('okay')
   }
 }
