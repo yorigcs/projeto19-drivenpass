a7b55a71ced5bb879d7e5e412c75f6bfefbfe4e7
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 20:41:58 -0300

    feat: create middleware to handle user authorization

---
+++ src/middlewares/authorizeUser.ts
@@ -0,0 +1,24 @@
+import { Response, Request, NextFunction } from 'express'
+import { TokenAdapter } from '../utils'
+
+interface JwtRequest extends Request {
+  headers: {
+    authorization: string
+  }
+}
+
+export class AuthorizeUser {
+  async handle (req: JwtRequest, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>>> {
+    const token = req.headers.authorization?.replace('Bearer ', '')
+    if (!token) {
+      return res.status(422).send('Must provide a token')
+    }
+    const tokenAdapter = new TokenAdapter()
+    const decoded = tokenAdapter.verifyToken(token)
+    if (!decoded) {
+      return res.status(401).send('Invalid Token')
+    }
+    res.locals.user_id = decoded.id
+    next()
+  }
+}
