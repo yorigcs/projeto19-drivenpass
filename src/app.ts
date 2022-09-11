3892a388e440d0afc5217cfc04c18972102cea3b
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-11 01:26:00 -0300

    feat: use authRouter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+
+import { authRouter } from './routes'
+
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use([cors(), express.json()])
-app.get('/', (req, res) => res.send('ok'))
+app.use([cors(), express.json(), authRouter])
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
