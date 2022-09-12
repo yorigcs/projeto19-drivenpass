4063b565f4cfde700e9b6e044465839db03743d8
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-12 15:02:16 -0300

    feat: implements createCardController to route

--- src/routes/cardsRoutes.ts
+++ src/routes/cardsRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 import { AuthorizeUser } from '../middlewares/authorizeUser'
-
+import { CardsCreateController } from '../controllers'
 const authorizeUser = new AuthorizeUser()
-
+const cardsCreateController = new CardsCreateController()
 const cardsRoutes = Router()
-cardsRoutes.post('/create-card', authorizeUser.handle)
+cardsRoutes.post('/create-card', authorizeUser.handle, cardsCreateController.handle)
 
 export { cardsRoutes }
